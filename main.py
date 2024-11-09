061c13b09370ef06e9d77b8d0acb2568c2a3dc4a
valentin` <lorishuon8@gmail.com>
2024-11-09 09:08:38 +0000

    creation code

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+def lire_invite():
+    while True:
+        texte = input("Écris quelque chose (ou tape 'exit' pour quitter) : ")
+        if texte.lower() == "exit":
+            print("Programme terminé.")
+            break
+        else:
+            print(f"Tu as écrit : {texte}")
 
+lire_invite()
