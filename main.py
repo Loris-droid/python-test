5c34ca9dbd01bece8dc6f6f34bd9c148e9c9ce5e
valentin` <lorishuon8@gmail.com>
2024-11-09 10:15:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+"""
+Ce module lit l'entrée de l'utilisateur et l'affiche dans la console.
+"""
+
 def lire_invite():
+    """Lit et affiche ce que l'utilisateur saisit jusqu'à ce que 'exit' soit saisi."""
     while True:
         texte = input("Écris quelque chose (ou tape 'exit' pour quitter) : ")
         if texte.lower() == "exit":
